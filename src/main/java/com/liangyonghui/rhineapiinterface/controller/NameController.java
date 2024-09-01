4ea9f8b73970c85d7a4dfba9497a99f6895856a2
梁永辉 <925277632@qq.com>
2024-09-01 19:53:37 +0800

    1

--- src/main/java/com/liangyonghui/rhineapiinterface/controller/NameController.java
+++ src/main/java/com/liangyonghui/rhineapiinterface/controller/NameController.java
@@ -55,6 +55,7 @@ public class NameController {
 //            throw new RuntimeException("无权限");
 //        }
         String result = "POST 用户名字是" + user.getUsername();
+        asd
         return result;
     }
 
