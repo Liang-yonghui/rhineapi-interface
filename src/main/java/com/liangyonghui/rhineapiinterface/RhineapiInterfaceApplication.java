5b6647858456955517d5e6ce50d9d6f601690eaa
梁永辉 <925277632@qq.com>
2024-09-01 19:38:44 +0800

    基本实现

---
+++ src/main/java/com/liangyonghui/rhineapiinterface/RhineapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.liangyonghui.rhineapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhineapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhineapiInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liangyonghui/rhineapiinterface/RhineapiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangyonghui.rhineapiinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhineapiInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
